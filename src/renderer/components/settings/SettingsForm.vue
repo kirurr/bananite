5ce65b9ce2b741bfa7890305e23cc36239b2dbe8
kirurr <timon09df@gmail.com>
2026-09-05 16:13:22 +0300

    feat: show toast in settings form

--- src/renderer/components/settings/SettingsForm.vue
+++ src/renderer/components/settings/SettingsForm.vue
@@ -3,15 +3,19 @@ import { ref, type Ref } from 'vue';
 import { useSystem } from '../../composables/system';
 import { useGame } from '../../composables/game';
 import DialogButtonInput from '../generic/DialogButtonInput.vue';
+import { useToast } from 'primevue/usetoast';
+import { handleErrorToast, TOAST_TIMEOUT_MS } from '../../composables/toast';
 
 const { openDialog } = useSystem();
+
+const toast = useToast();
 
 const { settings, setSettings } = useGame();
 
 const gamePath = ref(settings.value?.gamePath);
 const downloadPath = ref(settings.value?.downloadPath);
 
-async function handleChange(ref: Ref<string | undefined>): Promise<boolean> {
+async function handleDialog(ref: Ref<string | undefined>): Promise<boolean> {
   const string = await openDialog();
   if (string) {
     ref.value = string;
@@ -22,32 +26,61 @@ async function handleChange(ref: Ref<string | undefined>): Promise<boolean> {
 
 async function handleChangeGamePath(e: Event) {
   e.preventDefault();
-  const isChanged = await handleChange(gamePath);
+  const isChanged = await handleDialog(gamePath);
   if (!isChanged) return;
   if (!gamePath.value) return;
   if (!settings.value) await createNewSettings();
 
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  await setSettings({ ...settings.value!, gamePath: gamePath.value });
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    await setSettings({ ...settings.value!, gamePath: gamePath.value });
+  } catch (e) {
+    handleErrorToast(toast, e);
+    return;
+  }
+
+  toast.add({
+    severity: 'success',
+    summary: 'Success',
+    detail: 'Settings saved',
+    life: TOAST_TIMEOUT_MS,
+  });
 }
 
 async function handleChangeDownloadPath(e: Event) {
   e.preventDefault();
-  const isChanged = await handleChange(downloadPath);
+  const isChanged = await handleDialog(downloadPath);
 
   if (!isChanged) return;
   if (!downloadPath.value) return;
   if (!settings.value) await createNewSettings();
 
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  await setSettings({ ...settings.value!, downloadPath: downloadPath.value });
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    await setSettings({ ...settings.value!, downloadPath: downloadPath.value });
+  } catch (e) {
+    handleErrorToast(toast, e);
+    return;
+  }
+
+  toast.add({
+    severity: 'success',
+    summary: 'Success',
+    detail: 'Settings saved',
+    life: TOAST_TIMEOUT_MS,
+  });
 }
 
 async function createNewSettings() {
-  await setSettings({
-    gamePath: '',
-    downloadPath: '',
-  });
+  try {
+    await setSettings({
+      gamePath: '',
+      downloadPath: '',
+    });
+  } catch (e) {
+    handleErrorToast(toast, e);
+    return;
+  }
 }
 </script>
 
